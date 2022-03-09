841cac05de8eaf4f70d3cd2268a0ba195623a52d
FranciscoJesusGata <fjgatv8998@gmail.com>
2022-03-09 21:15:14 +0100

    mariadb setup fixed, wordpress in process...

--- srcs/requirements/mariadb/config/setup.sh
+++ srcs/requirements/mariadb/config/setup.sh
@@ -8,6 +8,8 @@ if [ ! -d /var/lib/mysql/mysql ]; then
 	chown -R mysql:mysql /var/lib/mysql
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql
 	cat << EOF > tmp.sql
+USE mysql;
+FLUSH PRIVILEGES;
 GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
 GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
 FLUSH PRIVILEGES ;
@@ -15,10 +17,10 @@ CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_ge
 GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD' ;
 FLUSH PRIVILEGES ;
 EOF
-	/usr/bin/mysqld --user=mysql < tmp.sql
+	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < tmp.sql
 	rm -f tmp.sql
 else
 	chown -R mysql:mysql /var/lib/mysql
 fi
 
-exec /usr/bin/mysqld --user=mysql
+exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0
---
+++ srcs/requirements/wordpress/conf/config_wp.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+file=/usr/share/webapps/wordpress/wp-config.php
+echo "<?php" > $file
+echo "define( 'DB_NAME', '$MYSQL_DATABASE' );" >> $file
+echo "define( 'DB_USER', '$MYSQL_USER' );" >> $file
+echo "define( 'DB_PASSWORD', '$MYSQL_PASSWORD' );" >> $file
+echo "define( 'DB_HOST', 'mariadb' );" >> $file
+echo "define( 'DB_CHARSET', 'utf8' );" >> $file
+echo "define( 'DB_COLLATE', '' );" >> $file
+wget -qO- https://api.wordpress.org/secret-key/1.1/salt/ >> $file
+echo "\$table_prefix = 'wp_';" >> $file
+echo "define( 'WP_DEBUG', false );" >> $file
+echo "if ( ! defined( 'ABSPATH' ) ) {" >> $file
+echo "	define( 'ABSPATH', __DIR__ . '/' );" >> $file
+echo "}" >> $file
+echo "require_once ABSPATH . 'wp-settings.php';" >> $file
